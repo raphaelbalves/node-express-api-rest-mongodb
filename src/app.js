61c862b61124a305c11a3f3bf372235011714605
Raphael B Alves <raphaelbarrosalves@gmail.com>
2025-09-10 06:21:05 -0300

    adicionada rota de livros

--- src/app.js
+++ src/app.js
@@ -2,8 +2,24 @@ import express from "express"
 
 const app = express()
 
+const livros = [
+    {
+        id: 1,
+        titulo: "O senhor dos anéis"
+    },
+
+    {
+        id: 2,
+        titulo: "O Hobbit"
+    }
+]
+
 app.get("/", (req, resp) => {
     resp.status(200).send("Estou usando Express no Node.js")
+})
+
+app.get("/livros", (req, resp) => {
+    resp.status(200).json(livros)
 })
 
 export default app
